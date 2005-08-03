6b80fdd8bcab1fb6c258351268fa535fc3c670dd
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2005-08-03 18:59:31 +0000

    clean up

--- test/testWriter.cc
+++ test/testWriter.cc
@@ -1,29 +1,43 @@
 #include "CondCore/DBCommon/interface/DBWriter.h"
 #include "CondFormats/Calibration/interface/Pedestals.h"
+#include "SealKernel/Service.h"
+#include "POOLCore/POOLContext.h"
+#include "SealKernel/Context.h"
 #include <string>
 #include <iostream>
 int main(){
+  pool::POOLContext::loadComponent( "SEAL/Services/MessageService" );
+  pool::POOLContext::setMessageVerbosityLevel( seal::Msg::Error );
   cond::DBWriter w(std::string("sqlite_file:test.db"));
-  Pedestals* ped=new Pedestals;
-  if( w.containerExists( "Pedestals" )){
-    std::cout<<"container Exisits"<<std::endl;
+  if( w.containerExists( "Pedestals" ) ){
+    Pedestals* ped1=new Pedestals;
+    std::cout<<"container exists"<<std::endl;
     w.openContainer("Pedestals");
     w.startTransaction();
     Pedestals::Item item;
     item.m_mean=0.1;
     item.m_variance=0.2;
-    ped->m_pedestals.push_back(item);
-    std::string tok=w.write<Pedestals>(ped);
+    ped1->m_pedestals.push_back(item);
+    std::string tok=w.write<Pedestals>(ped1);
+    Pedestals* ped2=new Pedestals;
+    ped2->m_pedestals.push_back(item);
+    w.write<Pedestals>(ped2);
     w.commitTransaction();
   }else{
-    std::cout<<1<<std::endl;
+    std::cout<<"container doesnot exist"<<std::endl;
     w.createContainer("Pedestals");
+    Pedestals* ped1=new Pedestals;
     w.startTransaction();
     Pedestals::Item item;
     item.m_mean=0.3;
     item.m_variance=0.4;
-    ped->m_pedestals.push_back(item);
-    std::string tok=w.write<Pedestals>(ped);
+    ped1->m_pedestals.push_back(item);
+    std::string tok=w.write<Pedestals>(ped1);
+    Pedestals* ped2=new Pedestals;
+    ped2->m_pedestals.push_back(item);
+    w.write<Pedestals>(ped2);
+    std::cout<<"about to commit"<<std::endl;
     w.commitTransaction();
+    std::cout<<"committed"<<std::endl;
   }
 }
