80c0a7200bba4bb9eccfecb5f1f9f95991ccac70
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2005-08-30 19:31:54 +0000

    adopt to new interface

--- test/testWriter.cc
+++ test/testWriter.cc
@@ -1,5 +1,6 @@
 #include "CondCore/DBCommon/interface/DBWriter.h"
 #include "CondFormats/Calibration/interface/Pedestals.h"
+#include "CondCore/IOVService/interface/IOV.h"
 #include "SealKernel/Service.h"
 #include "POOLCore/POOLContext.h"
 #include "SealKernel/Context.h"
@@ -9,35 +10,25 @@ int main(){
   pool::POOLContext::loadComponent( "SEAL/Services/MessageService" );
   pool::POOLContext::setMessageVerbosityLevel( seal::Msg::Error );
   cond::DBWriter w(std::string("sqlite_file:test.db"));
-  if( w.containerExists( "Pedestals" ) ){
-    Pedestals* ped1=new Pedestals;
-    std::cout<<"container exists"<<std::endl;
-    w.openContainer("Pedestals");
-    w.startTransaction();
-    Pedestals::Item item;
-    item.m_mean=0.1;
-    item.m_variance=0.2;
-    ped1->m_pedestals.push_back(item);
-    std::string tok=w.write<Pedestals>(ped1);
-    Pedestals* ped2=new Pedestals;
-    ped2->m_pedestals.push_back(item);
-    w.write<Pedestals>(ped2);
-    w.commitTransaction();
-  }else{
-    std::cout<<"container doesnot exist"<<std::endl;
-    w.createContainer("Pedestals");
-    Pedestals* ped1=new Pedestals;
-    w.startTransaction();
-    Pedestals::Item item;
-    item.m_mean=0.3;
-    item.m_variance=0.4;
-    ped1->m_pedestals.push_back(item);
-    std::string tok=w.write<Pedestals>(ped1);
-    Pedestals* ped2=new Pedestals;
-    ped2->m_pedestals.push_back(item);
-    w.write<Pedestals>(ped2);
-    std::cout<<"about to commit"<<std::endl;
-    w.commitTransaction();
-    std::cout<<"committed"<<std::endl;
-  }
+  w.startTransaction();
+  Pedestals* ped1=new Pedestals;
+  Pedestals::Item item;
+  item.m_mean=0.1;
+  item.m_variance=0.2;
+  ped1->m_pedestals.push_back(item);
+  std::string pedtok1=w.write<Pedestals>(ped1,"Pedestals");
+  Pedestals* ped2=new Pedestals;
+  ped2->m_pedestals.push_back(item);
+  std::string pedtok2=w.write<Pedestals>(ped2, "Pedestals");
+  cond::IOV* initiov=new cond::IOV;
+  initiov->iov.insert(std::make_pair(1,pedtok1));
+  initiov->iov.insert(std::make_pair(2,pedtok2));
+  std::string iovtok=w.write<cond::IOV>(initiov, "IOV");
+  w.commitTransaction();
+  w.startTransaction();
+  cond::IOV* bestiov=new cond::IOV;
+  bestiov->iov.insert(std::make_pair(10,pedtok1));
+  bestiov->iov.insert(std::make_pair(20,pedtok2));
+  std::string bestiovtok=w.write<cond::IOV>(bestiov, "IOV");
+  w.commitTransaction();
 }
