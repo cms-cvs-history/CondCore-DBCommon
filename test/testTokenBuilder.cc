17db09f58359aab9fdf04db3e26458a0cf01dd21
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2005-11-30 19:27:21 +0000

    update to seal::reflex

--- test/testTokenBuilder.cc
+++ test/testTokenBuilder.cc
@@ -6,14 +6,14 @@
 int main(){
   cond::TokenBuilder tk;
   try{
-    std::cout<<"build token by classid "<<std::endl;
-    tk.setDB("3E60FA40-D105-DA11-981C-000E0C4DE431");
-    tk.setContainer("2F16F0A9-79D5-4881-CE0B-C271DD84A7F1","Pedestals");
-    tk.setOID(0);
-    std::cout<<"result "<<tk.tokenAsString()<<std::endl;
-    std::cout<<"build token by dict "<<std::endl;
-    tk.setContainerFromDict("CondFormatsCalibration","Pedestals","Pedestals");
-    std::cout<<"result "<<tk.tokenAsString()<<std::endl;
+    tk.set("3E60FA40-D105-DA11-981C-000E0C4DE431",
+	   "CondFormatsCalibration",
+	   "Pedestals",
+	   "Pedestals",
+	   0);
+    std::cout<<"result 1 "<<tk.tokenAsString()<<std::endl;
+    tk.resetOID(2);
+    std::cout<<"result 2 "<<tk.tokenAsString()<<std::endl;
   }catch (const seal::Exception &e){
     std::cout << "test ERROR: "<< e.what() << std::endl;
   }catch (const std::exception &e) {
