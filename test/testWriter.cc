b908f9807633f7c6db1fbd509f041fb9faa0e026
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-02-16 10:30:14 +0000

    writer ok

--- test/testWriter.cc
+++ test/testWriter.cc
@@ -8,44 +8,51 @@
 #include <string>
 #include <iostream>
 int main(){
-  std::cout<<1<<'\n';
   cond::ServiceLoader* loader=new cond::ServiceLoader;
-  std::cout<<2<<'\n';
   loader->loadMessageService();
-  std::cout<<3<<'\n';
   cond::DBSession* session=new cond::DBSession(std::string("sqlite_file:test.db"));
-  std::cout<<4<<'\n';
   try{
-  session->connect(cond::ReadWriteCreate);
-  std::cout<<5<<'\n';
-  cond::DBWriter pwriter(*session, "Pedestals");
-  std::cout<<6<<'\n';
-  Pedestals* ped1=new Pedestals;
-  std::cout<<7<<'\n';
-  Pedestals::Item item;
-  item.m_mean=0.1;
-  item.m_variance=0.2;
-  ped1->m_pedestals.push_back(item);
-  std::cout<<8<<'\n';
-  session->startUpdate();
-  std::string pedtok1=pwriter.markWrite<Pedestals>(ped1);
-  Pedestals* ped2=new Pedestals;
-  ped2->m_pedestals.push_back(item);
-  std::string pedtok2=pwriter.markWrite<Pedestals>(ped2);
-  session->commit();
-  cond::IOV* initiov=new cond::IOV;
-  initiov->iov.insert(std::make_pair(1,pedtok1));
-  initiov->iov.insert(std::make_pair(2,pedtok2));
-  cond::DBWriter iovwriter(*session, "IOV");
-  std::string iovtok=iovwriter.markWrite<cond::IOV>(initiov);
-  session->commit();
-  session->startUpdate();
-  cond::IOV* bestiov=new cond::IOV;
-  bestiov->iov.insert(std::make_pair(10,pedtok1));
-  bestiov->iov.insert(std::make_pair(20,pedtok2));
-  std::string bestiovtok=iovwriter.markWrite<cond::IOV>(bestiov);
-  session->commit();
-  session->disconnect();
+    session->connect(cond::ReadWriteCreate);
+    cond::DBWriter pwriter(*session, "Pedestals");
+    cond::DBWriter iovwriter(*session, "IOV");
+    session->startUpdateTransaction();
+    ///init iov sequence
+    cond::IOV* initiov=new cond::IOV;
+    std::vector<std::string> pedtoks;
+    for(int i=0; i<4;++i){
+      Pedestals* ped=new Pedestals;
+      for(int ichannel=1; ichannel<=5; ++ichannel){
+	Pedestals::Item item;
+	item.m_mean=0.1*ichannel;
+	item.m_variance=0.2*ichannel;
+	ped->m_pedestals.push_back(item);
+      }
+      std::string pedtok=pwriter.markWrite<Pedestals>(ped);
+      pedtoks.push_back(pedtok);
+      if(i<2){
+	initiov->iov.insert(std::make_pair(1,pedtok));
+      }else{
+	initiov->iov.insert(std::make_pair(2,pedtok));
+      }
+    }
+    iovwriter.markWrite<cond::IOV>(initiov);
+    session->commit();
+    //create a new iov sequence
+    session->startUpdateTransaction();
+    cond::IOV* bestiov=new cond::IOV;
+    int counter=0;
+    for(std::vector<std::string>::iterator it=pedtoks.begin(); 
+	it!=pedtoks.end(); ++it){
+      ++counter;
+      if(counter<3){
+	bestiov->iov.insert(std::make_pair(1,*it));
+      }else{
+	bestiov->iov.insert(std::make_pair(2,*it));
+      }
+    }
+    std::string bestiovtok=iovwriter.markWrite<cond::IOV>(bestiov);
+    session->commit();
+    session->disconnect();
   }catch(cond::Exception& er){
     std::cout<<er.what()<<std::endl;
   }catch(seal::Exception& er){
