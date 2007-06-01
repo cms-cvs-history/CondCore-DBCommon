12f59f0a81a7b80422845eb238ba8188f06522a4
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2007-06-01 17:59:25 +0000

    added

---
+++ test/testDBCatalog.cc
@@ -0,0 +1,21 @@
+#include "CondCore/DBCommon/interface/DBCatalog.h"
+#include "FileCatalog/IFileCatalog.h"
+#include <string>
+#include <iostream>
+int main(){
+  cond::DBCatalog mycat;
+  std::string lfn("/dev/CMS_COND_GENERAL");
+  bool isLFN=mycat.isLFN(lfn);
+  std::cout<<lfn<<" is LFN"<<std::endl;
+  std::cout<<"cat name "<<mycat.defaultDevCatalogName()<<std::endl;
+  if(isLFN){
+    mycat.poolCatalog().setWriteCatalog(mycat.defaultDevCatalogName());
+    mycat.poolCatalog().connect();
+    mycat.poolCatalog().start();
+    std::string pf=mycat.getPFN(mycat.poolCatalog(),lfn, false);
+    mycat.poolCatalog().commit();
+    mycat.poolCatalog().disconnect();
+    std::cout<<"pfn "<<pf<<std::endl;
+  }
+  return 0;
+}
