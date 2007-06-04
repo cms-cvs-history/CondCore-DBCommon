276bb5bd5d5a3a9f5a8210c9c54650918b8d7982
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2007-06-04 15:01:49 +0000

    add frontier test case

--- test/testDBCatalog.cc
+++ test/testDBCatalog.cc
@@ -14,8 +14,12 @@ int main(){
     mycat.poolCatalog().start();
     std::string pf=mycat.getPFN(mycat.poolCatalog(),lfn, false);
     mycat.poolCatalog().commit();
-    mycat.poolCatalog().disconnect();
     std::cout<<"pfn "<<pf<<std::endl;
+    mycat.poolCatalog().start();
+    pf=mycat.getPFN(mycat.poolCatalog(),lfn,true);
+    mycat.poolCatalog().commit();
+     std::cout<<"pfn with cache "<<pf<<std::endl;
+    mycat.poolCatalog().disconnect();
   }
   return 0;
 }
