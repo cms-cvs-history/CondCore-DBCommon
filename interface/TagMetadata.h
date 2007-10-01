d77f21f9e56cd739815bc31a5169de1e991ce55e
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2007-10-01 11:54:35 +0000

    added

---
+++ interface/TagMetadata.h
@@ -0,0 +1,12 @@
+#ifndef DBCommon_TagMetadata_h
+#define DBCommon_TagMetadata_h
+#include <string>
+namespace cond{
+  class TagMetadata{
+  public:
+    std::string recordname;
+    std::string objectname;
+    std::string labelname;
+  };
+}
+#endif
