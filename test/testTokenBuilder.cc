21559176e553fdd16ef0ef8cd1855f73608aaf8c
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2008-01-28 10:45:17 +0000

    add TokenInterpreter utility and unittest

--- test/testTokenBuilder.cc
+++ test/testTokenBuilder.cc
@@ -1,21 +1,31 @@
 #include "CondCore/DBCommon/interface/TokenBuilder.h"
+#include "CondCore/DBCommon/interface/TokenInterpreter.h" 
 #include "CondCore/DBCommon/interface/Exception.h"
 #include <string>
 #include <iostream>
 int main(){
   cond::TokenBuilder tk;
+  std::string tok1,tok2;
+  
   try{
     tk.set("3E60FA40-D105-DA11-981C-000E0C4DE431",
 	   "testDict",
 	   "testCondObj",
 	   "testCondObj",
 	   0);
-    std::cout<<"result 1 "<<tk.tokenAsString()<<std::endl;
+    tok1=tk.tokenAsString();
+    std::cout<<"result 1 "<<tok1<<std::endl;
     tk.resetOID(2);
-    std::cout<<"result 2 "<<tk.tokenAsString()<<std::endl;
+    tok2=tk.tokenAsString();
+    std::cout<<"result 2 "<<tok2<<std::endl;
   }catch (const cond::Exception &e){
     std::cout << "test ERROR: "<< e.what() << std::endl;
   }catch(...){
     std::cout<<"funny error"<<std::endl;
   }
+
+  cond::TokenInterpreter tokenteller(tok1);
+  std::string containerName=tokenteller.containerName();
+  std::string className=tokenteller.className();
+  std::cout<<"token "<<tok1<<" belongs to container "<<containerName<<" class "<<className<<std::endl;
 }
