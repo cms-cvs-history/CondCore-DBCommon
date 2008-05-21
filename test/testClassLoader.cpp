7e0448be9ccb40363c59e8f1e852efa93e69000e
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-05-21 15:42:34 +0000

    compiles

--- test/testClassLoader.cpp
+++ test/testClassLoader.cpp
@@ -8,13 +8,13 @@
 std::string const tokens[] = {
   "[DB=00000000-0000-0000-0000-000000000000][CNT=CSCPedestalsRcd][CLID=E1D4BE86-63E6-21C8-41B3-1116BCFBDE24][TECH=00000B01][OID=00000004-00000003]",
     "[DB=00000000-0000-0000-0000-000000000000][CNT=Fake][CLID=E1D4BE86-0000-21C8-41B3-1116BCFBDE24][TECH=00000B01][OID=00000004-00000003]"
-    }
+    };
 
 size_t N=2;
 
 int main() {
   
-  for (size_t i=0; i<N) {
+  for (size_t i=0; i<N; i++) {
     std::string const & token = tokens[i];
     const pool::Guid guid(cond::classID(token));
     ROOT::Reflex::Type type = pool::DbReflex::forGuid(guid);
