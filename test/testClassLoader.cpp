30c0e426152713581c6beb051fda6f3b32e83264
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2008-05-22 06:26:02 +0000

    better

--- test/testClassLoader.cpp
+++ test/testClassLoader.cpp
@@ -9,10 +9,11 @@
 
 std::string const tokens[] = {
   "[DB=00000000-0000-0000-0000-000000000000][CNT=CSCPedestalsRcd][CLID=E1D4BE86-63E6-21C8-41B3-1116BCFBDE24][TECH=00000B01][OID=00000004-00000003]",
-    "[DB=00000000-0000-0000-0000-000000000000][CNT=Fake][CLID=E1D4BE86-0000-21C8-41B3-1116BCFBDE24][TECH=00000B01][OID=00000004-00000003]"
+    "[DB=00000000-0000-0000-0000-000000000000][CNT=Fake][CLID=E1D4BE86-0000-21C8-41B3-1116BCFBDE24][TECH=00000B01][OID=00000004-00000003]",
+  "[DB=00000000-0000-0000-0000-000000000000][CNT=CSCPedestalsRcd][CLID=E1D4BE86-63E6-21C8-41B3-1116BCFBDE24][TECH=00000B01][OID=00000004-00000005]"
     };
 
-size_t N=2;
+size_t N=3;
 
 int main() {
 
@@ -20,15 +21,7 @@ int main() {
   
   for (size_t i=0; i<N; i++) {
     std::string const & token = tokens[i];
-    const pool::Guid guid(cond::classID(token));
-    ROOT::Reflex::Type type = pool::DbReflex::forGuid(guid);
-    if (!type) {
-      if (!cond::loadClassByToken(token)) 
-	std::cout << "dict unknown for " << token << std::endl;
-      type = pool::DbReflex::forGuid(guid);
-      if (!type)  
-	std::cout << "problem with DbReflex for " << token << std::endl;
-    }
+    ROOT::Reflex::Type const & reflexTypeByToken(token);
     std::cout << "class " << type.Name(ROOT::Reflex::SCOPED)
 	      << " for " << token << std::endl;
   }
